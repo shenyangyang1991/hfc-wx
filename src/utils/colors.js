a6315eddcd271a34c7495c7d419dded7dab15d46
shenyangyang <shenyangyang@daling.com>
2018-06-20 19:13:18 +0800

    commit

---
+++ src/utils/colors.js
@@ -0,0 +1,9 @@
+// const colors = ['#800080', '#FFFF00', '#FF0000', '#800000', '#808000', '#00FF00', '#008000', '#00FFFF', '#008080', '#FF00FF']
+
+const colors = ['#e3214d', '#31b6e1']
+
+export default function () {
+  // let rand = Math.random() * 10 << 0
+  let rand = 0
+  return colors[rand]
+}
