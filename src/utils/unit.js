93f57d9092acedd9301e09dd189065de1f86e39a
shenyangyang <shenyangyang@daling.com>
2018-07-20 15:54:49 +0800

    add

--- src/utils/unit.js
+++ src/utils/unit.js
@@ -1,3 +1,10 @@
-export default function(size) {
-  return (size / 1000).toFixed(2)
+const units = ['', '万', '亿'];
+export const int2unit = (int) => {
+  const k = 10000;
+  const i = Math.floor(Math.log(int) / Math.log(k));
+  if (i > 0) {
+    return (int / Math.pow(k, i)).toPrecision(3) + '' + units[i];
+  }
+
+  return int;
 }
